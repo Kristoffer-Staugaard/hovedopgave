a02942d319d262718ef19c28be7036b7478be718
khni48704 <khni48704@edu.ucl.dk>
2024-05-10 16:10:34 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import './assets/main.css'
 import { createApp } from 'vue';
 import header from './components/header.vue';
 const headerUCL = createApp(header);
-headerUCL.mount('#header'); // Mount the app on element with id="app" in index.html
+headerUCL.mount('#header'); // Mount the app on element with id="heaer" in index.html
 
 
 import footer from './components/footer.vue';
 const footerUCL = createApp(footer);
-footerUCL.mount('#footer'); // Mount the app on element with id="app" in index.html
+footerUCL.mount('#footer'); // Mount the app on element with id="footer" in index.html
