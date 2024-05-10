641ada9c9bcc7773721680cbb00ce76a2ed30004
khni48704 <khni48704@edu.ucl.dk>
2024-05-10 15:50:09 +0200

    1

--- src/main.js
+++ src/main.js
@@ -6,10 +6,5 @@ import header from './components/header.vue';
 const app = createApp(header);
 app.mount('#app'); // Mount the app on element with id="app" in index.html
 
-import { createApp } from 'vue';
-import footer from './components/footer.vue';
-const app1 = createApp(footer);
-app.mount('#app1'); // Mount the app on element with id="app" in index.html
-
 
 
