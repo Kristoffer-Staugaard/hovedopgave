53a473c6fc4ff804830e144eae4b77f3d1ae9f61
khni48704 <khni48704@edu.ucl.dk>
2024-05-10 15:43:37 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@ import './assets/main.css'
 // main.js
 import { createApp } from 'vue';
 import header from './components/header.vue';
-const app = createApp(header);
-
-const app = createApp(footer);
 import footer from './components/footer.vue';
+const app = createApp(header);
+const app = createApp(footer);
 app.mount('#app'); // Mount the app on element with id="app" in index.html
 
