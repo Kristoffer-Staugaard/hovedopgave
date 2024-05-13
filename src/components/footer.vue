d165fcd2669c35754d58b698c00d4ce5a014a27e
Edisavdagic <119938495+Edisavdagic@users.noreply.github.com>
2024-05-13 10:36:57 +0200

    Update footer.vue

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,12 +1,45 @@
 <template> 
-<div class="Footer">Dette er Footer.</div> 
+<footer>
+    <div> 
+        <ul>
+            <li>Studieservice</li> 
+            <li>Events</li>
+            <li>Studieliv</li>                  
+        </ul>
+    </div>
+    <div> 
+        <ul>
+            <li>Biblotek</li> 
+            <li>Tilgængelighedserklæring</li>            
+        </ul>
+    </div>
+    <div> 
+        <ul>
+            <li>mitUCL.dk er et website for studerende på UCL.</li> 
+            <br>
+            <li class="footer-about">UCL Erhvervsakademi og Professionshøjskole udbyder
+                43 uddannelser i Odense, Svendborg, Vejle, Fredericia og Jelling. 
+                Som studerende kommer du tæt på virkeligheden og vil i fremtiden 
+                bidrage til bæredygtig vækst, velfærd og løsninger, der gør en forskel.</li>            
+        </ul>
+    </div>
+
+
+
+</footer>
 </template>
 
 <style>
-.Footer{
-    border: 1px solid darkgray;
-    background-color: orange;
-    padding: 50px
+footer{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.footer-about{
+    width: 50%;
+
+
 
 }
 </style>
