63d51c9dab35744885bbbbc27cfc443a0032f392
khni48704 <khni48704@edu.ucl.dk>
2024-05-14 13:44:08 +0200

    reviews

--- src/components/reviews.vue
+++ src/components/reviews.vue
@@ -9,39 +9,40 @@ onMounted(() => {
 });
 
 const getReviwsSection = () => {
-  fetch('https://hovedopgave-f875e-default-rtdb.firebaseio.com/reviws.json', {
+  fetch('https://hovedopgave-f875e-default-rtdb.firebaseio.com/reviews.json', {
     method: 'GET'
   })
   .then((res) => res.json())
   .then((res) => {
     console.log(res);
-    rewies.value = res;
+    reviews.value = res;
   })
   .catch((err) => {
     console.log(err);
     error.value = 'Kunne ikke hente data';
   });
 };
+
 </script>
 
 <template>
-  <section id="reviws_section">
-    <div class="reviws_container">
-      <div v-for="(  reviws, index) in reviews" :key="index" class="reviws_box"> 
+  <section id="reviews_section">
+    <div class="reviews_container">
+      <div v-for="(  reviews, index) in reviews" :key="index" class="reviews_box"> 
         <div>
-              <img id="reviws_img" :src="reviews.image" alt="reviws billede" class="reviws_image" />
+              <img id="reviews_img" :src="reviews.image" alt="reviews billede" class="reviews_image" />
           </div>
         <div>
-          <p class="reviws_education">{{ reviws.education }}</p> 
+          <p class="reviews_education">{{ reviews.education }}</p> 
         </div>  
         <div>
-          <p class="reviws_message">{{ reviws.message }}</p> 
+          <p class="reviews_message">{{ reviews.message }}</p> 
         </div> 
         <div>
-          <p class="reviws_name">{{ reviews.name }}</p> 
+          <p class="reviews_name">{{ reviews.name }}</p> 
         </div>
         <div>
-          <p class="reviws_titel">{{ reviews.titel }}</p> 
+          <p class="reviews_titel">{{ reviews.titel }}</p> 
         </div>  
       </div>
     </div>
