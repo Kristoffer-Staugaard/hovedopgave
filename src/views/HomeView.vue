3fabff6cdb84e66ad2dbc309554962deb2a83b85
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2024-05-14 11:10:44 +0200

    små ændringer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
+import { ref } from 'vue';
 import headerUCL from '../components/headerUCL.vue';
 
 </script>
 
 <template>
+    <p>hej</p>
 <headerUCL />
 </template>
---
+++ src/views/Karrierevejledning.vue
@@ -0,0 +1,8 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <p>hej</p>
+</template>
