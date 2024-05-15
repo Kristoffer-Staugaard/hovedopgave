b17c45c33780ed7940e13be250b993ad63427ba1
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2024-05-15 10:14:24 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@ import headerUCL from "../components/headerUCL.vue";
 import navigation from "@/components/navigation.vue";
 import KVstudiestart from "@/components/KVstudiestart.vue";
 import KVUdannelsensOpbygning from "@/components/KVUdannelsensOpbygning.vue";
-import KVpraktik from @/components/KVpraktik.vue";
+import KVpraktik from "@/components/KVpraktik.vue";
 </script>
 
 <template>
