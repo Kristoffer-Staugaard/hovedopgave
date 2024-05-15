7f4e9428eff4d53c712f64169b567ef44c6f9b69
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2024-05-15 10:05:31 +0200

    kv

---
+++ src/components/KVpraktik.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <h2>Praktik</h2>
+    <div class="buttom-divider">
+      <div>
+        <p>Dit praktikforløb er af 3 måneders varighed.</p>
+        <ul>
+          <li>Praktikken finder sted på 4. semester.</li>
+          <li>Du er selv ansvarlig for at finde en praktikplads.</li>
+          <li>Praktikperioden er ulønnet og SU-berettiget</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script scoped></script>
+
+<style scoped></style>
