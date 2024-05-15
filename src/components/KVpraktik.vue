cd98f0074f3ff9c8b4cd19af25d049b045e02518
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2024-05-15 10:31:40 +0200

    kv

--- src/components/KVpraktik.vue
+++ src/components/KVpraktik.vue
@@ -1,8 +1,26 @@
+<script scoped>
+import { ref } from "vue";
+
+const openIndex = ref(null);
+
+const dropdowns = ref([
+  { dropdown: "En til en vejledning", answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  { dropdown: "Vejledning i klassen/hold", answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  { dropdown: "Karrierevejledning i grupper", answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  { dropdown: "CV-tjek", answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
+  { dropdown: "Digitale guides", answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }
+]);
+
+function toggleDropdown(index) {
+  openIndex.value = openIndex.value === index ? null : index;
+}
+</script>
+
 <template>
   <section>
     <h2>Praktik</h2>
     <div class="buttom-divider">
-      <div>
+      <div class="buttom-divider-width">
         <p>Dit praktikforløb er af 3 måneders varighed.</p>
         <ul>
           <li>Praktikken finder sted på 4. semester.</li>
@@ -10,10 +28,36 @@
           <li>Praktikperioden er ulønnet og SU-berettiget</li>
         </ul>
       </div>
+      <div class="buttom-divider-width">
+        <div class="item-wrap">
+        <div class="dropdown-wrap" v-for="(item, index) in dropdowns">
+          <div class="dropdown" @click="toggleDropdown(index)">
+            <h3>{{ item.dropdown }}</h3>
+            <span>{{ openIndex === index ? '-' : '+' }}</span>
+          </div>
+          <div class="answer" v-if="openIndex === index">
+            <p>{{ item.answer }}</p>
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
   </section>
 </template>
 
-<script scoped></script>
+<style scoped>
+dropdown-wrap {
+  border: solid 2px #CAE4E3;
+  margin-bottom: 20px;
+}
 
-<style scoped></style>
+.dropdown {
+  display: flex;
+  justify-content: space-between;
+  padding: 25px;
+}
+
+.answer {
+  padding: 25px;
+}
+</style>
