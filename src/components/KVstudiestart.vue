95dd6ba779d0e4eeac8e6cfa2c89af1caf5ed54d
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2024-05-15 09:32:11 +0200

    kvstudie

---
+++ src/components/KVstudiestart.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <div class="buttom-divider">
+        <h1>Multimediedesigner</h1>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style scoped>
+    .buttom-divider {
+        padding-bottom: 40px;
+        border-bottom: 2px #E6F1F0 solid;
+    }
+
+</style>
