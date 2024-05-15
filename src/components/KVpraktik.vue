29db6b4c8c8ad04a487b8270db68508fe4a301d6
Kristoffer-Staugaard <kristofferstaugaard@gmail.com>
2024-05-15 11:03:36 +0200

    kv

--- src/components/KVpraktik.vue
+++ src/components/KVpraktik.vue
@@ -1,14 +1,34 @@
-<script scoped>
+<script setup>
 import { ref } from "vue";
 
 const openIndex = ref(null);
 
 const dropdowns = ref([
-  { dropdown: "En til en vejledning", answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
-  { dropdown: "Vejledning i klassen/hold", answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
-  { dropdown: "Karrierevejledning i grupper", answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
-  { dropdown: "CV-tjek", answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." },
-  { dropdown: "Digitale guides", answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." }
+  {
+    dropdown: "Om praktikken",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    dropdown: "Jobportalen",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    dropdown: "Praktikfaktaark og praktikportal",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    dropdown: "Ikke gennemført praktik",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    dropdown: "Få hjælp",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
 ]);
 
 function toggleDropdown(index) {
@@ -28,36 +48,55 @@ function toggleDropdown(index) {
           <li>Praktikperioden er ulønnet og SU-berettiget</li>
         </ul>
       </div>
-      <div class="buttom-divider-width">
-        <div class="item-wrap">
-        <div class="dropdown-wrap" v-for="(item, index) in dropdowns">
-          <div class="dropdown" @click="toggleDropdown(index)">
-            <h3>{{ item.dropdown }}</h3>
-            <span>{{ openIndex === index ? '-' : '+' }}</span>
+        <div class="buttom-divider-width">
+          <div class="item-wrap">
+            <div class="dropdown-wrap" v-for="(item, index) in dropdowns">
+              <div class="dropdown" @click="toggleDropdown(index)">
+                <h3>{{ item.dropdown }}</h3>
+                <span>{{ openIndex === index ? "-" : "+" }}</span>
+              </div>
+              <div class="answer" v-if="openIndex === index">
+                <p>{{ item.answer }}</p>
+              </div>
+            </div>
           </div>
-          <div class="answer" v-if="openIndex === index">
-            <p>{{ item.answer }}</p>
+    </div>
+    </div>
+    <div class="buttom-divider">
+      <div class="blue-card">
+        <h3>Kontakt din praktikkoordinator</h3>
+        <img src="../components/praktikkoordinator.png" alt="Prkatik koordinator profil billede">
+        <div class="koordinator-info">
+          <h4>Karen Malene Andreasen</h4>
+          <p>Praktikkordinator</p>
+          <div class="list">
+            
           </div>
         </div>
-      </div>
       </div>
     </div>
   </section>
 </template>
-
 <style scoped>
-dropdown-wrap {
-  border: solid 2px #CAE4E3;
-  margin-bottom: 20px;
+.dropdown-wrap {
+  margin-top: 5px;
+  background-color: #daeced;
+  width: 350px;
 }
 
 .dropdown {
   display: flex;
   justify-content: space-between;
-  padding: 25px;
+  padding: 15px;
 }
 
 .answer {
-  padding: 25px;
+  padding: 15px;
+}
+
+.buttom-divider-wrap {
+  display: flex;
+  width: 100%;
+  gap: 60px;
 }
 </style>
